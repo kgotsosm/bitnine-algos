97adf5571d74e63cafe7154d908084853d440b74
Kgotso Makhalimele <89485688+kgotsosm@users.noreply.github.com>
2023-08-22 18:03:18 +0200

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,15 +1,24 @@
+/**
+ * Display a digital clock in the console.
+ * The clock format is: hh:mm:ss AM/PM
+ */
 function myDigitalClock() {
+    // Get the current date and time
     var d1 = new Date();
+
+    // Extract hours, minutes, and seconds from the date
     var hours = d1.getHours(); // 0 - 23
     var minutes = d1.getMinutes(); // 0 - 59
     var seconds = d1.getSeconds(); // 0 - 59
-    var zone = "AM";
 
-    // Leading seconds for minutes and seconds less than 10
+    // Initialize the time zone indicator as "AM" or "PM"
+    var zone = (hours >= 12) ? "PM" : "AM";
+
+    // Add leading zeros for minutes and seconds less than 10
     minutes = (minutes < 10) ? "0" + minutes : minutes;
     seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-    // Creating digital clock format (hh:mm:ss AM/PM)
+    // Create the digital clock format (hh:mm:ss AM/PM)
     let digitalClock = hours + ":" + minutes + ":" + seconds + " " + zone;
 
     // Display the digital clock in the console
@@ -18,3 +27,4 @@ function myDigitalClock() {
 
 // Call the myDigitalClock function to start the digital clock
 myDigitalClock();
+
